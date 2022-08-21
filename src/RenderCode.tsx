1028c4f69f0ca4b2b96ec0bebba48bb2680b3f47
Royston Shufflebotham <royston@shufflebotham.org>
2022-08-21 20:43:46 +0100

    Simplify styling classes

--- src/RenderCode.tsx
+++ src/RenderCode.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import type { TOTP } from 'otpauth';
 
 import classes from './RenderCode.module.scss';
@@ -15,37 +16,23 @@ export function RenderCode(props: IRenderCodeProps) {
   const thisCode = useGenerateCode(otp, periodNumber);
   const nextCode = useGenerateCode(otp, periodNumber + 1);
 
-  const switchOver = 3;
+  const switchOver = 5;
 
   const width = `${(100 * remainingTime) / otp.period}%`;
+
+  const switchingOver = remainingTime < switchOver;
+
   return (
-    <div>
-      Remaining: {periodNumber} {remainingTime}; code: {thisCode} -&gt;{' '}
-      {nextCode}
+    <div className={switchingOver ? classes.switchingOver : ''}>
+      Remaining time: {remainingTime}
       <div className={classes.timerOuterLozenge}>
         <div
           className={classes.timerInnerLozenge}
           style={{ width: width }}
         ></div>
         <div className={classes.codesContainer}>
-          <div
-            className={
-              classes.thisCode +
-              ' ' +
-              (remainingTime < switchOver ? classes.slideOut : '')
-            }
-          >
-            {thisCode}
-          </div>
-          <div
-            className={
-              classes.nextCode +
-              ' ' +
-              (remainingTime < switchOver ? classes.slideIn : '')
-            }
-          >
-            {nextCode}
-          </div>
+          <div className={classes.thisCode}>{thisCode}</div>
+          <div className={classes.nextCode}>{nextCode}</div>
         </div>
       </div>
     </div>
