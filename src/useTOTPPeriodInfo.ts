29d8de16f9463f590f1b17a2f65f9f646fa41828
Royston Shufflebotham <royston@shufflebotham.org>
2024-11-09 17:26:43 +0000

    Fix initial remaining time display

--- src/useTOTPPeriodInfo.ts
+++ src/useTOTPPeriodInfo.ts
@@ -2,7 +2,9 @@ import type { TOTP } from 'otpauth';
 import { useEffect, useMemo, useState } from 'react';
 
 export function useTOTPPeriodInfo(period: number) {
-  const [remainingTime, setRemainingTime] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(() =>
+    getRemainingTime(period),
+  );
   const [periodNumber, setPeriodNumber] = useState(() =>
     getPeriodNumber(period),
   );
@@ -10,8 +12,7 @@ export function useTOTPPeriodInfo(period: number) {
   useEffect(() => {
     const handle = setInterval(() => {
       const newPeriodNumber = getPeriodNumber(period);
-      const remainingTime =
-        ((newPeriodNumber + 1) * period * 1000 - Date.now()) / 1000;
+      const remainingTime = getRemainingTime(period);
       setPeriodNumber(newPeriodNumber);
       setRemainingTime(remainingTime);
     }, 500);
@@ -34,4 +35,11 @@ export function useGenerateCode(otp: TOTP, periodNumber: number) {
 function getPeriodNumber(period: number) {
   const now = Date.now();
   return Math.floor(now / (period * 1000));
+}
+
+function getRemainingTime(period: number) {
+  const newPeriodNumber = getPeriodNumber(period);
+  const remainingTime =
+    ((newPeriodNumber + 1) * period * 1000 - Date.now()) / 1000;
+  return remainingTime;
 }
