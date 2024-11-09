9f0afb77df17918a8defa421d8e76561ede298f0
Royston Shufflebotham <royston@shufflebotham.org>
2024-11-09 13:20:31 +0000

    Remove StrictMode in production build

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,15 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(
+
+const rootEl = import.meta.env.PROD ? (
+  <App />
+) : (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+root.render(rootEl);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
